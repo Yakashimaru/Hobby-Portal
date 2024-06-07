f29b505ad95e4e9f72d86bf315f02eecc46a4adf
Joel John Tan <114053402+Yakashimaru@users.noreply.github.com>
2024-06-08 02:19:52 +0800

    Added "update dates" functionality for VN

---
+++ Backend/README.md
@@ -0,0 +1,4 @@
+# Setup
+## Generating requirements.txt
+1) CD to backend
+2) Enter `pipreqs .` into the cli
--- Backend/common/database_functions.py
+++ Backend/common/database_functions.py
@@ -2,11 +2,8 @@ from flask import Flask, request, jsonify
 
 from ..settings.database_connection import connect_to_database
 from ..common.handle_exceptions import internal_error_exception
+from ..common.data_functions import convert_keys_values_to_string
 
-def get_keys_and_values(data):
-    keys = ", ".join(map(str, data.keys()))
-    values = ", ".join(map(str, data.values()))
-    return keys, values
 
 def get_database(table_name):
     try:
@@ -22,6 +19,31 @@ def get_database(table_name):
 
         conn.commit()
         cur.close()
+        conn.close()
+
+        result = {'columns': columns, 'rows': rows}
+
+        return jsonify(result)
+
+    except Exception as e:
+        return internal_error_exception(e)
+    
+
+def get_database_inner_join(table_name1, table_name2, join_clause ,order = "*", sort_by_clause = ""):
+    try:
+        conn = connect_to_database()
+        cur = conn.cursor()
+        
+        sql_query = f"SELECT {order} FROM {table_name1} INNER JOIN {table_name2} ON {join_clause} {sort_by_clause}"
+        cur.execute(sql_query)
+
+        rows = cur.fetchall()
+
+        columns = [desc[0] for desc in cur.description]
+
+        conn.commit()
+        cur.close()
+        conn.close()
 
         result = {'columns': columns, 'rows': rows}
 
@@ -36,16 +58,23 @@ def post_database(table_name, data):
         conn = connect_to_database()
         cur = conn.cursor()
 
-        column_names, column_values = get_keys_and_values(data)
+        #column_names, column_values = get_keys_and_values(data)
+
+        column_names, column_values = convert_keys_values_to_string(data.keys(), data.values())
+
+        # Split the comma-separated string of values into a list
+        column_values_list = [v.strip() for v in column_values.split(',')]
+
+        # Replace empty strings or "null" values with None
+        for i, value in enumerate(column_values_list):
+            if value.strip() == "" or value.strip().lower() == "null":
+                column_values_list[i] = None
 
         # Construct the SQL query with placeholders for values
-        sql_query = f"INSERT INTO {table_name} ({column_names}) VALUES ({', '.join(['%s'] * len(column_values.split(',')))})"
-
-        # Split column values string into individual values
-        values = [v.strip() for v in column_values.split(',')]
+        sql_query = f"INSERT INTO {table_name} ({column_names}) VALUES ({', '.join(['%s'] * len(column_values_list))})"
 
         # Execute the query with column values as parameters
-        cur.execute(sql_query, values)
+        cur.execute(sql_query, column_values_list)
 
         conn.commit()
         cur.close()
@@ -60,23 +89,30 @@ def post_database(table_name, data):
         return internal_error_exception(e)
 
 
-def put_database(table_name, data):
+def put_database(table_name, data, condition_column):
     try:
         conn = connect_to_database()
         cur = conn.cursor()
         
-        # sql_query = f"UPDATE {table_name} SET ({update_query}) WHERE ({', '.join(['%s'] * len(column_values.split(',')))})"
-        # cur.execute(sql_query, (table_name, update_query, where_clause))
-        #cur.execute("UPDATE datacamp_courses SET course_instructor = 'Izzy Weber' WHERE course_name = 'Introduction to SQL'")
+        # Build the SET part of the query dynamically
+        set_clause = ', '.join([f"{key} = %s" for key in data.keys() if key != condition_column])
+        condition_value = data[condition_column]
+        del data[condition_column]
+
+        # Create the SQL query
+        sql_query = f"UPDATE {table_name} SET {set_clause} WHERE {condition_column} = %s"
+        
+        # Execute the SQL query
+        cur.execute(sql_query, (*data.values(), condition_value))
 
         conn.commit()
         cur.close()
         conn.close()
 
         return jsonify({
-            "code": 200,
+            "code": 201,
             "message": "Data updated successfully"
-        }), 200
+        }), 201
     
     except Exception as e:
         return internal_error_exception(e)
