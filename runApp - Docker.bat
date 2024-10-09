9477920de6a08af09d19f591e33a87632edbcaab
Joel John Tan <114053402+Yakashimaru@users.noreply.github.com>
2024-10-09 23:07:39 +0800

    Update runApp - Docker.bat
    Add minimize for bat

--- runApp - Docker.bat
+++ runApp - Docker.bat
@@ -4,7 +4,7 @@ REM Activate virtual environment
 cd /d "%~dp0"
 
 REM Run docker compose
-start cmd /k "docker-compose up"
+start /min cmd /k "docker-compose up"
 
 REM Wait a few seconds to allow services to start
 timeout /t 5 /nobreak >nul
